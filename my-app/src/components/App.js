baf312ee674e4abea8162dc7f7c4038c767b7ec6
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-08-20 09:51:59 +0200

    e

---
+++ my-app/src/components/App.js
@@ -0,0 +1,39 @@
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+import {
+  Profile,
+  Statistics,
+  FriendList,
+  TransactionHistory,
+} from '../components';
+
+const rootStyle = {
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  color: '#010101',
+};
+
+export const App = () => {
+  return (
+    <div style={rootStyle}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.followers}
+        views={user.views}
+        likes={user.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+    </div>
+  );
+};
