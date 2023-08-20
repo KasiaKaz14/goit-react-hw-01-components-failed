308fd6826c73463b46411d4ba54216b2466f1de7
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-08-20 10:12:59 +0200

    app finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
-import './App.css';
-import { App } from './components/App.js';
+// import './App.css';
+// import { App } from './components/App.js';
 
 // function App() {
 //   return (
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -15,3 +15,11 @@ FriendListItem.propTypes = {
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
 };
+
+{
+  /* <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li> */
+}
